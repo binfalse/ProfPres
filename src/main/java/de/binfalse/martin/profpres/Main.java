a96be71abadb7adc612747f5923b15044f1bb744
Martin Scharm <martin@binfalse.de>
2015-01-25 09:17:47 +0100

    just comments

--- src/main/java/de/binfalse/martin/profpres/Main.java
+++ src/main/java/de/binfalse/martin/profpres/Main.java
@@ -27,57 +27,6 @@ public class Main
 	 */
 	public static void main (String[] args)
 	{
-		/*char [] a = new char [] {'A', 'B', 'C'};
-		//int n = 5;
-		int j = 1;
-		//if (j < 0)
-		//	j = n + j;
-		//int r = j;
-		for (int i = 0; i < a.length; i++)
-			System.out.println ((a.length + j + i) + ": " + a[i] + " -> " + a[(a.length + j + i) % a.length]);
-		
-		
-
-		if (1==1) return;*/
-		/*try {
-      UIManager.setLookAndFeel("com.seaglasslookandfeel.SeaGlassLookAndFeel");
-  } catch (Exception e) {
-  	e.printStackTrace ();
-		try {
-	    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-	      if ("Nimbus".equals(info.getName())) {
-	          UIManager.setLookAndFeel(info.getClassName());
-	          break;
-	      }
-	  }
-	} catch (Exception e4) {
-	  // If Nimbus is not available, you can set the GUI to another look and feel.
-			try
-			{
-				UIManager.setLookAndFeel (UIManager.getSystemLookAndFeelClassName ());
-			}
-			catch (Exception e1)
-			{
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-	}}/*UIDefaults defaults = UIManager.getDefaults();
-  //defaults.putDefaults(newSettings);
-  defaults.put("Button.background", Color.BLACK);*/
-  
-  
-  
-
-    /*Hashtable   properties = UIManager.getDefaults();
-    properties.put ("Panel.background" , Color.BLACK);
-    
-    Enumeration keys       = properties.keys();
-
-    while (keys.hasMoreElements()) {
-      String key   = keys.nextElement().toString ();
-      Object value = properties.get (key);
-      System.out.println(key + "\t" + value);
-    }*/
   
 		try {
 	    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
